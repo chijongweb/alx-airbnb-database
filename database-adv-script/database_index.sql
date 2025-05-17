8d49ab5c80f23a49cfdcc737823f4871eb34e78b
chijongweb <160375788+chijongweb@users.noreply.github.com>
2025-05-17 05:34:35 +0200

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,1 +1,11 @@
+-- Indexes on User table
+CREATE UNIQUE INDEX idx_user_email ON users(email);
 
+-- Indexes on Booking table
+CREATE INDEX idx_booking_user ON bookings(user_id);
+CREATE INDEX idx_booking_property ON bookings(property_id);
+CREATE INDEX idx_booking_date ON bookings(booking_date);
+
+-- Indexes on Property table
+CREATE INDEX idx_property_host ON properties(host_id);
+CREATE INDEX idx_property_city ON properties(city);
