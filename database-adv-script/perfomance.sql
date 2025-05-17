6646df7f45d6a516d1ef94d9ca2953f710aca528
chijongweb <160375788+chijongweb@users.noreply.github.com>
2025-05-17 05:46:19 +0200

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,1 +1,14 @@
-
+SELECT
+    b.booking_id,
+    b.booking_date,
+    u.user_id,
+    u.name AS user_name,
+    p.property_id,
+    p.address AS property_address,
+    pay.payment_id,
+    pay.amount AS payment_amount,
+    pay.payment_date
+FROM bookings b
+INNER JOIN users u ON b.user_id = u.user_id
+INNER JOIN properties p ON b.property_id = p.property_id
+INNER JOIN payments pay ON b.booking_id = pay.booking_id;
