6712642194eeebac75fcbb84cfccf77abc17e673
chijongweb <160375788+chijongweb@users.noreply.github.com>
2025-05-17 05:22:18 +0200

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,1 +1,34 @@
+-- 1️ Non-Correlated Subquery: Find all properties where the average rating is greater than 4.0
 
+SELECT 
+    property_id, 
+    property_name
+FROM 
+    properties
+WHERE 
+    property_id IN (
+        SELECT 
+            property_id 
+        FROM 
+            reviews 
+        GROUP BY 
+            property_id 
+        HAVING 
+            AVG(rating) > 4.0
+    );
+
+
+-- 2️ Correlated Subquery: Find users who have made more than 3 bookings
+
+SELECT 
+    user_id, 
+    name
+FROM 
+    users u
+WHERE 
+    (SELECT 
+        COUNT(*) 
+     FROM 
+        bookings b 
+     WHERE 
+        b.user_id = u.user_id) > 3;
