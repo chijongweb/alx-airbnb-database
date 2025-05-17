27688387627404b08d6f2aacd2e6be4eb9316210
chijongweb <160375788+chijongweb@users.noreply.github.com>
2025-05-17 06:00:11 +0200

    Update partitioning.sql

--- database-adv-script/partitioning.sql
+++ database-adv-script/partitioning.sql
@@ -1,1 +1,20 @@
+CREATE TABLE bookings_partitioned (
+    booking_id INT NOT NULL,
+    user_id INT NOT NULL,
+    property_id INT NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE,
+    -- other columns...
+    PRIMARY KEY (booking_id, start_date)
+)
+PARTITION BY RANGE (YEAR(start_date)) (
+    PARTITION p2019 VALUES LESS THAN (2020),
+    PARTITION p2020 VALUES LESS THAN (2021),
+    PARTITION p2021 VALUES LESS THAN (2022),
+    PARTITION p2022 VALUES LESS THAN (2023),
+    PARTITION pmax VALUES LESS THAN MAXVALUE
+);
 
+
+INSERT INTO bookings_partitioned
+SELECT * FROM bookings;
